ea7c54915b67a2db01047ad02ef624bcd4b777f9
Mahesh S <37013197+Mahesh2877@users.noreply.github.com>
2022-05-12 19:58:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,30 +11,38 @@ PASSWORD = getpass("Enter the password: ")
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("https://www.linkedin.com/uas/login")
+# Visit the url showing the list of connection requests
+driver.get('https://www.linkedin.com/mynetwork/invitation-manager/')
 
 # to maximize the browser window
 driver.maximize_window()
 
+#Click on "Sign In"
+sign_in_link = driver.find_element_by_class_name("main__sign-in-link")
+sign_in_link.click()
+
+# Enter the login credentials and hit ENTER
 email=driver.find_element_by_id("username")
 email.send_keys(USERNAME)
+
 password=driver.find_element_by_id("password")
 password.send_keys(PASSWORD)
-sleep(5)
+
 password.send_keys(Keys.RETURN)
-print("Entered login credentials")
 
-#get method to launch the URL
-driver.get('https://www.linkedin.com/mynetwork/invitation-manager/')
-print("Openned network lists!!!!")
-#to refresh the browser
-#driver.refresh()
+# Find & Click on the "people" button to go to list of people's requests
 sleep(5)
+people_button = driver.find_element_by_id("mn-invitation-manager__invitation-facet-pills--CONNECTION")
+people_button.click()
 
-button = driver.find_element_by_xpath("//button[@class='artdeco-button artdeco-button--2 artdeco-button--secondary ember-view invitation-card__action-btn']")
+# Extract the list of "Accept" buttons for display
+sleep(5)
+accept_buttons = driver.find_elements_by_class_name("artdeco-button artdeco-button--2 artdeco-button--secondary ember-view invitation-card__action-btn")
+# display.display(len(accept_button))
+for individual_button in accept_buttons:
+    individual_button.click()
 
-button.click()
-
+# Sleep for a while
 sleep(10)
 
-driver.close()
+# driver.close()
