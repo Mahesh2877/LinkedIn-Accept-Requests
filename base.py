24a5848a2d0f9f18c7aba82b57872194d61781c4
Mahesh S <37013197+Mahesh2877@users.noreply.github.com>
2022-05-12 20:08:03 +0530

    Update and rename test.py to base.py

--- base.py
+++ base.py
@@ -1,3 +1,4 @@
+from IPython import display
 from selenium import webdriver
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,15 +7,18 @@ from selenium.webdriver.common.keys import Keys
 from getpass import getpass
 from selenium.webdriver import ActionChains
 
+# Ask the user for the username and password.
+# getpass() ensures that the password will not be visible during input
 USERNAME = input("Enter the username: ")
 PASSWORD = getpass("Enter the password: ")
 
+# Load the Chrome browser before openning it
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 # Visit the url showing the list of connection requests
 driver.get('https://www.linkedin.com/mynetwork/invitation-manager/')
 
-# to maximize the browser window
+# Maximize the browser window
 driver.maximize_window()
 
 #Click on "Sign In"
@@ -30,19 +34,22 @@ password.send_keys(PASSWORD)
 
 password.send_keys(Keys.RETURN)
 
+# First sleep for a while to give time for the elements to load...
+# ...If we don't sleep, I've noticed that the code fails in the following line
 # Find & Click on the "people" button to go to list of people's requests
 sleep(5)
 people_button = driver.find_element_by_id("mn-invitation-manager__invitation-facet-pills--CONNECTION")
 people_button.click()
 
-# Extract the list of "Accept" buttons for display
+# Use xpath to find the list of Accept buttons to Click
+# The syntax in this line basically says to look for the <button> tag first...
+# ...then find the "class" attribute having the exact same value as given here
 sleep(5)
-accept_buttons = driver.find_elements_by_class_name("artdeco-button artdeco-button--2 artdeco-button--secondary ember-view invitation-card__action-btn")
-# display.display(len(accept_button))
+accept_buttons = driver.find_elements_by_xpath("//button[@class='artdeco-button artdeco-button--2 artdeco-button--secondary ember-view invitation-card__action-btn']")
+# Iterate through the list of buttons and start CLICKING 'EM!!!'
 for individual_button in accept_buttons:
     individual_button.click()
 
-# Sleep for a while
-sleep(10)
-
-# driver.close()
+# Sleep for a while before closing the window
+sleep(5)
+driver.close()
